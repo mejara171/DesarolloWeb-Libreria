67aa51d96f82159c4f685766a0f36f5ef32fd487
Martin <martin.zuluaga@upb.edu.co>
2022-09-23 13:34:21 -0500

    3.2 Convert to Roman

---
+++ Ejercicios/5. First JS/text.js
@@ -0,0 +1,54 @@
+function convertToRoman(num) 
+{
+
+    if (num == 1 ) 
+    {
+        return 'I'
+    }
+
+    if (num == 2 ) 
+    {
+        return 'II'
+    }
+
+    if (num == 3 ) 
+    {
+        return 'III'
+    }
+
+    if (num == 4 ) 
+    {
+        return 'IV'
+    }
+
+    if (num == 5 ) 
+    {
+        return 'V'
+    }
+
+    if (num == 6 ) 
+    {
+        return 'VI'
+    }
+
+    if (num == 7 ) 
+    {
+        return 'VII'
+    }
+
+    if (num == 8 ) 
+    {
+        return 'VIII'
+    }
+
+    if (num == 9 ) 
+    {
+        return 'IX'
+    }
+
+
+    else {return 'Not implemented'}
+    
+  
+
+  }
